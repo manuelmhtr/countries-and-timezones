c2dc96da53569d8bd26617612419319bc0fa18b9
Sachin Singh <ssingh259@sapient.com>
2021-05-16 17:30:41 +0530

    Added esm build

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,43 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import { uglify } from "rollup-plugin-uglify";
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'umd',
-    name: 'ct',
-    sourcemap: true
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'ct',
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs(),
+      json(),
+      babel({
+        babelHelpers: 'bundled',
+        presets: ['@babel/env'],
+      }),
+      uglify(),
+    ],
   },
-  plugins: [
-    commonjs(),
-    json(),
-    babel({
-      babelHelpers: 'bundled',
-      presets: ['@babel/env']
-    }),
-    uglify()
-  ],
-};
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      name: 'ct',
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs(),
+      json(),
+      babel({
+        babelHelpers: 'bundled',
+        presets: ['@babel/env'],
+      }),
+      uglify(),
+    ],
+  },
+];
